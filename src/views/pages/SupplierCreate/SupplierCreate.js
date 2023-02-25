c3eccfd16ad398fb5f1072e0c4b3e1edad7ea51b
sohrab09 <sohrab@nassagroup.org>
2023-02-25 15:43:33 +0600

    Supplire Create Form Done without validation

--- src/views/pages/SupplierCreate/SupplierCreate.js
+++ src/views/pages/SupplierCreate/SupplierCreate.js
@@ -1,6 +1,74 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Post } from '../../../http/http';
 
 const SupplierCreate = () => {
+
+    // All States Here
+    const [supplierId, setSupplierId] = useState('');
+    const [fullName, setFullName] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    // const [image, setImage] = useState('');
+    const [note, setNote] = useState('');
+
+    // All Handlers Here
+    const handleSupplierId = (e) => {
+        setSupplierId(e.target.value)
+    };
+
+    const handleFullName = (e) => {
+        setFullName(e.target.value)
+    };
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    };
+
+    const handlePhone = (e) => {
+        setPhone(e.target.value)
+    };
+
+    const handleNote = (e) => {
+        setNote(e.target.value)
+    };
+
+
+    // Clear Inputs
+    const clearInputs = () => {
+        setSupplierId('');
+        setFullName('');
+        setEmail('');
+        setPhone('');
+        setNote('');
+    }
+
+    // Create Supplier
+    const createSupplier = async () => {
+        const data = {
+            supplierId: supplierId,
+            fullName: fullName,
+            email: email,
+            phone: phone,
+            note: note
+        }
+
+        console.log("data --------->>>>>>>> ", data)
+
+        try {
+            await Post('api/Supplier/Add', data)
+                .then(res => {
+                    console.log("res --------->>>>>>>> ", res)
+                    if (res.data.statusCode === 200) {
+                        alert(res.data.message)
+                        clearInputs()
+                    }
+                })
+        } catch (error) {
+            console.log("Supplier Create Error ----->>>>>", error)
+            alert(error.response.data.message)
+        }
+    }
+
     return (
         <div className="row">
             <div className="col-sm-12 col-lg-12">
@@ -11,69 +79,70 @@ const SupplierCreate = () => {
                         </div>
                     </div>
                     <div className="iq-card-body">
-                        <form
-                            className="needs-validation"
-                            novalidate
-                        >
+                        <div className="needs-validation">
                             <div className="form-row">
                                 <div className="col-md-6 mb-3">
-                                    <label for="validationCustom02">Supplier ID</label>
+                                    <label htmlFor="supplierId">Supplier ID</label>
                                     <input
                                         type="text"
                                         className="form-control"
-                                        id="validationCustom02"
-                                        required
+                                        id="supplierId"
+                                        value={supplierId}
+                                        onChange={handleSupplierId}
                                     />
                                 </div>
                                 <div className="col-md-6 mb-3">
-                                    <label for="validationCustom01">Full Name</label>
+                                    <label htmlFor="fullName">Full Name</label>
                                     <input
                                         type="text"
                                         className="form-control"
-                                        id="validationCustom01"
-                                        required
+                                        id="fullName"
+                                        value={fullName}
+                                        onChange={handleFullName}
                                     />
                                 </div>
                                 <div className="col-md-6 mb-3">
-                                    <label for="validationCustom03">Email</label>
+                                    <label htmlFor="email">Email</label>
                                     <input
                                         type="email"
-                                        id="validationCustom03"
                                         className="form-control"
-                                        required
+                                        id="email"
+                                        value={email}
+                                        onChange={handleEmail}
                                     />
                                 </div>
                                 <div className="col-md-6 mb-3">
-                                    <label for="validationCustom04">Phone</label>
+                                    <label htmlFor="phone">Phone</label>
                                     <input
                                         type="number"
                                         className="form-control"
-                                        id="validationCustom04"
-                                        required
+                                        id="phone"
+                                        value={phone}
+                                        onChange={handlePhone}
                                     />
                                 </div>
                                 <div className="col-md-6 mb-3">
-                                    <label for="validationCustom04">Supplier Photo</label>
+                                    <label htmlFor="supplierPhoto">Supplier Photo (Not Mandatory)</label>
                                     <input
                                         type="file"
                                         className="form-control"
-                                        id="validationCustom04"
-                                        required
+                                        id="supplierPhoto"
                                     />
                                 </div>
                                 <div className="col-md-6 mb-3">
-                                    <label for="validationCustom04">Note</label>
+                                    <label htmlFor="note">Note</label>
                                     <textarea
                                         className="form-control"
-                                        id="validationTextarea"
                                         placeholder="Any Special Note"
-                                        required
+                                        id="note"
+                                        value={note}
+                                        onChange={handleNote}
                                     >
                                     </textarea>
                                 </div>
                             </div>
-                            <button className="btn btn-primary" type="submit">Submit</button>
-                        </form>
+                            <button className="btn btn-primary" onClick={createSupplier}>Submit</button>
+                        </div>
                     </div>
                 </div>
             </div>
