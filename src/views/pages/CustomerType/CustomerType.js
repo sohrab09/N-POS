f20a29d56ad10d9995c69d64a73850225b211db2
sohrab09 <sohrab@nassagroup.org>
2023-03-04 16:01:59 +0600

    Customer Type GET done

--- src/views/pages/CustomerType/CustomerType.js
+++ src/views/pages/CustomerType/CustomerType.js
@@ -1,13 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './CustomerType.css';
+import Table from 'react-bootstrap/Table';
+import { Get } from '../../../http/http';
 
 
 const CustomerType = () => {
 
 
+  const [loading, setLoading] = useState(false)
+  const [getCustomerType, setGetCustomerType] = useState([]);
+
+  useEffect(() => {
+    setLoading(true)
+    const getAllCustomerTypeData = () => {
+      try {
+        Get('api/CustomerType/GetAll')
+          .then((res) => {
+            console.log("Get All Customer Type ------>>>>> ", res);
+            if (res.data.statusCode === 200) {
+              setLoading(false)
+              setGetCustomerType(res.data.data);
+            }
+          })
+      } catch (error) {
+        setLoading(false)
+        console.log("Get All Customer Type Error ----->>>>>", error)
+      }
+    };
+    getAllCustomerTypeData();
+  }, []);
+
   return (
     <div className="container-fluid p-0 m-0">
-      <h1>CustomerType</h1>
+      {
+        loading === true ?
+          <div className="d-flex justify-content-center align-items-center h-100">
+            <div className="spinner-border" role="status">
+              <span className="visually-hidden">Loading...</span>
+            </div>
+          </div>
+          :
+          <Table>
+            <thead style={{ backgroundColor: '#704cb6', color: '#fff', fontSize: '15px', fontWeight: 'bolder' }}>
+              <tr>
+                <th>Customer Type</th>
+                <th>Discount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                getCustomerType.map((item, index) => {
+                  return (
+                    <tr key={index}>
+                      <td>{item.name}</td>
+                      <td>{item.discount}</td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </Table>
+      }
     </div>
 
   );
